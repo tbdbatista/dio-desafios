93fc98af2973fdc716a6bc220bfdc351a7399e6f
Thiago Beltrami Dias Batista <tbdbatista@gmail.com>
2024-07-09 11:28:21 -0300

    Implementa desafio do contador com excecao

---
+++ processoseletivo/contador/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package processoseletivo.contador;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
+
